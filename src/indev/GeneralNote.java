854ab428586c59ee9f41d93e8d3e63a79d179440
zgw <micoael@qq.com>
2017-12-17 09:59:12 +0800

    The note

---
+++ src/indev/GeneralNote.java
@@ -0,0 +1,44 @@
+package indev;
+import java.util.Date;
+import java.util.Random;
+
+public class GeneralNote {
+	private long UID=0 ;
+	private String evtTitle;
+	private String evtDesc;
+	private Date evtDate;
+	Random rand= new Random();
+	
+	public GeneralNote(){
+		setUID(rand.nextLong());
+		
+	}
+	
+	public void setUID(long uid){
+		this.UID=uid;
+	}
+	
+	public void setEvtTitle(String EvtTitle) {
+		this.evtTitle=EvtTitle;
+	}
+	
+	public void setEvtDesc(String EvtDesc) {
+		this.evtDesc=EvtDesc;
+	}
+	
+	public long getUID(){
+		return this.UID;
+	}
+	
+	public String getEvtTitle(){
+		return this.evtTitle;
+	}
+	
+	public String getEvtDesc(){
+		return this.evtDesc;
+	}
+	
+	public Date getDate(){
+		return this.evtDate;
+	}
+}
