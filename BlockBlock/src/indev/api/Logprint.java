ffd3aaea7951db295944f020056218a50e4d5f22
zgw <micoael@qq.com>
2017-12-28 11:08:12 +0800

    Update Logprint.java

--- BlockBlock/src/indev/api/Logprint.java
+++ BlockBlock/src/indev/api/Logprint.java
@@ -3,6 +3,6 @@ package indev.api;
 public class Logprint {
 	public void prlog(String Mfrom,String msg) {
 		
-		System.out.println("["Mfrom+"]>> ["+msg+"]");
+		System.out.println("["+Mfrom+"]>> ["+msg+"]");
 	}
 }
